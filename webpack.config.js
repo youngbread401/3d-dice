e744490fe027a3115bd3cacdb6c5d6c4ced4fcbb
Daniel <daniel.a.lakin@gmail.com>
2025-01-12 12:46:08 -0500

    Update build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      dangerouslyLinkNativeLibraries: true
+    },
+    removeUnusedImports: false,
+  }, argv);
   
   // Customize the config before returning it.
   config.resolve.fallback = {
     ...config.resolve.fallback,
     "fs": false,
-    "path": false
+    "path": false,
+    "crypto": false
   };
 
   return config;
